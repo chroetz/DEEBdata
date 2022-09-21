f51b1d08c8173327c5868e984c0ccd87653be7c0
Christof Schoetz <cschoetz@pik-potsdam.de>
2022-09-21 15:41:47 +0200

    add NULL as return

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,10 @@
 .onLoad <- function(libname, pkgname) {
+
   paths <- getOption("ConfigOpts.pathDefaults")
   thisPath <- system.file("defaultOpts", package=pkgname, lib.loc=libname)
   if (!thisPath %in% paths) {
     options(ConfigOpts.pathDefaults = c(thisPath, paths))
   }
-  invisible()
+
+  invisible(NULL)
 }
