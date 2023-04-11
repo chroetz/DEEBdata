0cc49fce47b708da827e354652a851e723d99397
Christof Schoetz <cschoetz@pik-potsdam.de>
2023-04-11 20:58:13 +0200

    use more DEEButil

--- R/funPolynomial.R
+++ R/funPolynomial.R
@@ -1,9 +1,6 @@
 getParmsFunctionPolynomial <- function(opts) {
 
-  p <- numberOfTermsInPoly(opts$polyDeg, opts$d)
-  degVecs <- as.matrix(expand.grid(rep(list(0:opts$polyDeg), opts$d)))
-  degVecs <- degVecs[rowSums(degVecs) <= opts$polyDeg, , drop=FALSE]
-  stopifnot(nrow(degVecs) == p)
+  degVecs <- DEEButil::getMonomialExponents(d, opts$polyDeg)
 
   parmsFunction <- function(t, u, parms) {
     if (any(abs(u) > opts$boundary)) return(list(rep(0, length(u))))
@@ -19,12 +16,7 @@ getParmsFunctionPolynomial <- function(opts) {
 buildParmsSamplerPolynomial <- function(opts) {
   coefSampler <- buildArraySampler(
     opts$coefSampler,
-    arrayDim = c(opts$d, numberOfTermsInPoly(opts$polyDeg, opts$d)))
+    arrayDim = c(opts$d, DEEButil::numberOfTermsInPoly(opts$polyDeg, opts$d)))
   parmsSampler <- function() list(coef = coefSampler())
   return(parmsSampler)
-}
-
-
-numberOfTermsInPoly <- \(polyDeg, d) {
-  sum(sapply(0:polyDeg, \(deg) choose(d+deg-1, deg)))
 }
