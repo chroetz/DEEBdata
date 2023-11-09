f00359aecad138ee3bbf89a9f5b859191c5dce9d
Christof Schoetz <cschoetz@pik-potsdam.de>
2023-11-09 09:06:41 +0100

    randomize seed

--- R/run.R
+++ R/run.R
@@ -1,9 +1,18 @@
 #' @export
-run <- function(x, createExample = FALSE, overwriteList = NULL, truth = TRUE, obs = TRUE, task = TRUE, plot = TRUE) {
+run <- function(
+    x,
+    overwriteList = NULL,
+    truth = TRUE, obs = TRUE, task = TRUE, plot = TRUE,
+    randomizeSeed = FALSE) {
 
   opts <- asOpts(x, "Run")
 
   opts <- overwriteOpts(opts, overwriteList)
+
+  if (randomizeSeed) {
+    opts$truth$seed <- sample.int(.Machine$integer.max, 1)
+    opts$observation$seed <- sample.int(.Machine$integer.max, 1)
+  }
 
   wd <- getwd()
   on.exit(setwd(wd))
@@ -26,21 +35,54 @@ run <- function(x, createExample = FALSE, overwriteList = NULL, truth = TRUE, ob
   if (obs) generateObservations(opts$observation, writeOpts = FALSE)
   if (task) writeTasks(opts$taskList, writeOpts = FALSE)
   if (plot) plotTogether(opts$plot, writeOpts = FALSE)
+}
 
-  if (createExample) {
-    message("Creating Example")
-    dir.create("example")
-    setwd("example")
-    opts$truth$seed <- sample.int(.Machine$integer.max, 1)
-    opts$observation$seed <- sample.int(.Machine$integer.max, 1)
-    writeOpts(opts, "Opts_Run")
-    if (truth) sampleTrajectoriesAndWriteForTasks(
-      opts$truth,
-      opts$taskList,
-      opts$observation,
-      writeOpts = FALSE)
-    if (obs) generateObservations(opts$observation, writeOpts = FALSE)
-    if (task) writeTasks(opts$taskList, writeOpts = FALSE)
-    if (plot) plotTogether(opts$plot, writeOpts = FALSE)
+
+#' @export
+getRunOptsFiles <- function(
+    fullPath = FALSE,
+    fileEnding = fullPath,
+    optsPath = NULL,
+    fromPackage = TRUE
+) {
+  if (length(optsPath) == 0) optsPath <- ""
+  if (fromPackage) {
+    runOptsPath <- system.file(
+      file.path("runOpts", optsPath),
+      package = "DEEBdata")
+  } else {
+    runOptsPath <- normalizePath(optsPath, mustWork=TRUE)
   }
+  files <- dir(runOptsPath, full.names=fullPath)
+  if (!fileEnding)
+    files <- substr(files, 1, nchar(files) - 5)
+  return(files)
+}
+
+#' @export
+runAll <- function(
+    dbPath = paste0("~/DEEBDB", reps),
+    pattern = NULL,
+    optsPath = NULL,
+    fromPackage = TRUE,
+    overwrite = NULL,
+    reps = NULL,
+    truth = TRUE, obs = TRUE, task = TRUE, plot = TRUE,
+    randomizeSeed = FALSE
+) {
+  files <- getRunOptsFiles(fullPath = TRUE, optsPath = optsPath, fromPackage = fromPackage)
+  if (!is.null(pattern)) files <- files[grepl(pattern, basename(files))]
+  overwrite <- c(overwrite, list(path = dbPath))
+  if (!is.null(reps)) {
+    overwrite$truth <- c(overwrite$truth, list(reps = reps))
+  }
+  for (fl in files) {
+    run(
+      fl,
+      overwriteList = overwrite,
+      truth = truth, obs = obs, task = task, plot = plot,
+      randomizeSeed = randomizeSeed)
+  }
+  dbPath <- normalizePath(dbPath)
+  return(dbPath)
 }
